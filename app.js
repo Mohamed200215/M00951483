fe5d8d31230c7aa0b55d76f63aac216317265838
Mohamed200215 <maxamedmosseabdi@gmail.com>
2025-10-13 00:02:18 +0100

    Added cart button

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ createApp({
     return {
       siteName: "After School Classes",
       lessons: [
-        { id: 1, subject: "Math", location: "Hendon", price: 100, image: "images/math.jpg" },
-        { id: 2, subject: "Science", location: "Colindale", price: 90, image: "images/science.jpg" },
-        { id: 3, subject: "Music", location: "Brent Cross", price: 80, image: "images/music.jpg" }
+        { id: 1, subject: "Math", location: "Location: Hendon", price: 100, image: "images/math.jpg" },
+        { id: 2, subject: "Science", location: "Location: Colindale", price: 90, image: "images/science.jpg" },
+        { id: 3, subject: "Music", location: "Location: Brent Cross", price: 80, image: "images/music.jpg" }
       ]
     };
   },

6fdba4d103c5d27cfbb2de1f518b28e9aec90eb7
Mohamed200215 <maxamedmosseabdi@gmail.com>
2025-10-12 23:22:27 +0100

    Improved page design with banner and color layout

--- app.js
+++ app.js
@@ -18,9 +18,9 @@ createApp({
       // Array of lessons shown as boxes
       lessons: [
         // Each object below is one lesson box
-        { id: 1, subject: "Math", location: "Hendon", price: 100, image: "images/math.jpg" },
-        { id: 2, subject: "Science", location: "Colindale", price: 90, image: "images/science.jpg" },
-        { id: 3, subject: "Music", location: "Brent Cross", price: 80, image: "images/music.jpg" }
+        { id: 1, subject: "Math", location: "Location: Hendon", price: 100, image: "images/math.jpg" },
+        { id: 2, subject: "Science", location: "Location: Colindale", price: 90, image: "images/science.jpg" },
+        { id: 3, subject: "Music", location: "Location: Brent Cross", price: 80, image: "images/music.jpg" }
       ]
     };
   }
